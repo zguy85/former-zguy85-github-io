d62195a21ad39657a7029cf83e15c6443190980c
Z Lee <jzuriel85@gmail.com>
2017-05-05 16:04:53 -0400

    Update firstBatchScript.bat
    Add section to use variables

--- batch/firstBatchScript.bat
+++ batch/firstBatchScript.bat
@@ -2,4 +2,14 @@
 Rem This script will list all the files in the directory and put them
 Rem in a separate file
 dir "C:\Program Files" > C:\lists.txt
-echo "The program has completed"
+echo "The first part of the program has completed"
+
+
+Rem This script shows the use of variables
+set globalvar = 5 
+SETLOCAL 
+set var = 13145 
+set /A var = %var% + 5 
+ENDLOCAL 
+echo %var% 
+echo %globalvar%
